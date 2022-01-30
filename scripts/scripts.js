e95a8f59428e73ff71cb19be774a4962f03b7294
Eduardo Nagashima <eduyudji@gmail.com>
2022-01-29 23:34:07 -0300

    calculando certo preços

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,13 +1,4 @@
-let nomePrato = null;
-let precoPrato = null;
-
-let nomeBebida = null;
-let precoBebida = null;
-
-let nomeSobremesa = null;
-let precoSobremesa = null;
-
-let soma = 0;
+let nomePrato, preciPrato, nomeBebida, precoBebida, nomeSobremesa, precoSobremesa, soma = null;
 
 function selecionarPrato(pratoEscolhido, nome, preco) {
     let prato = document.querySelector("." + pratoEscolhido);
@@ -16,7 +7,7 @@ function selecionarPrato(pratoEscolhido, nome, preco) {
         selecionado.classList.remove("selecionado");
     }
     nomePrato = nome;
-    precoPrato = preco;
+    precoPrato = preco.toFixed(2);
     prato.classList.add("selecionado");
     checkout();
 }
@@ -30,7 +21,7 @@ function selecionarBebida(bebidaEscolhida, nome, preco) {
     }
     bebida.classList.add("selecionado");
     nomeBebida = nome;
-    precoBebida = preco;
+    precoBebida = preco.toFixed(2);
     checkout();
 }
 
@@ -42,35 +33,49 @@ function selecionarSobremesa(sobremesaEscolhida, nome, preco) {
     }
     sobremesa.classList.add('selecionado');
     nomeSobremesa = nome;
-    precoSobremesa = preco;
+    precoSobremesa = preco.toFixed(2);
     checkout();
 }
 
 function checkout() {
     if (nomePrato !== null && nomeBebida !== null && nomeSobremesa !== null) {
-
         let confirmar = document.querySelector(".confirmar");
         confirmar.classList.add('fechar-pedido');
         confirmar.innerHTML = 'Fechar Pedido';
-
     }
 }
 
 function fecharPedido() {
-    soma = 0;
+    // resetando o valor total da compra.
     let fecharPedido = document.querySelector('.fechar-pedido');
 
     if (fecharPedido !== null) {
-        soma = (precoBebida + precoPrato + precoSobremesa);
-        soma = soma.toFixed(2);
+        soma = parseFloat(precoBebida) + parseFloat(precoPrato) + parseFloat(precoSobremesa);
+
+        prato = document.querySelector('.prato-pedido .prato');
+        prato.innerHTML = nomePrato;
+
+        valorPrato = document.querySelector('.prato-pedido .precoPrato');
+        valorPrato.innerHTML = ('R$ ' + precoPrato);
+
+        bebida = document.querySelector('.bebida-pedido .bebida');
+        bebida.innerHTML = nomeBebida;
+
+        valorBebida = document.querySelector('.bebida-pedido .precoBebida');
+        valorBebida.innerHTML = ('R$ ' + precoBebida);
+
+        sobremesa = document.querySelector('.sobremesa-pedido .sobremesa');
+        sobremesa.innerHTML = nomeSobremesa;
+
+        valorSobremesa = document.querySelector('.sobremesa-pedido .precoSobremesa');
+        valorSobremesa.innerHTML = ('R$ ' + precoSobremesa);
+
+        precoTotal = document.querySelector('.total-pedido .precoTotal');
+        precoTotal.innerHTML = ('R$ ' + soma.toFixed(2));
+
         let escondido = document.querySelector('.escondido');
         escondido.classList.remove('escondido');
-
-
-        return soma;
     }
-
-
 }
 
 function cancelar() {
